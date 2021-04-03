b181438390d429a1a41ad36409740b63402cbc43
Sakthikumar.S <59757069+sakthikumarselvaraj@users.noreply.github.com>
2021-04-03 11:28:40 +0530

    Add files via upload

---
+++ contracts/Election.sol
@@ -0,0 +1,50 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    bool goingon = true;
+    mapping(address => bool) public voters;
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        addCandidate("Candidate 3");
+        addCandidate("Candidate 4");
+        addCandidate("Candidate 5");
+        addCandidate("Candidate 6");
+        
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function end () public {
+        goingon = false;
+    }
+
+    function vote (uint _candidateId) public {
+        require(!voters[msg.sender],"Already voted");
+
+        require(_candidateId > 0 && _candidateId <= candidatesCount,"Invalid candidate");
+
+        require(goingon,"Election ended");
+
+        voters[msg.sender] = true;
+
+        candidates[_candidateId].voteCount ++;
+
+        emit votedEvent(_candidateId);
+    }
+}
